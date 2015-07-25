374921d477660e786fcccb809aba211e24fcc4ab
JackyShen <mebusw@163.com>
2015-07-25 10:44:05 +0800

    more directives as 'element' and 'attribute'

--- app/js/directives.js
+++ app/js/directives.js
@@ -13,5 +13,24 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(bsoi);
     };
-  }])
+  }]).
+  directive('discount', function() {
+    console.log('discount=');
+    return {
+      restrict: "A",
+      transclude: true,
+      template: "<div><div ng-transclude></div>Discounted!</div>",
+      replace: false,
+      link: function (scope, element, attr) {
+        console.log(['LINKing...', scope,element,attr]);
+      }
+    };
+  }).
+  directive('custom', function() {
+    console.log('custom directive');
+    return {
+      restrict: "E",
+      template: "<div>Custom Directive</div>",
+    };
+  })
   ;
